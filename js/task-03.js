f1a2826a988c80f9009f28965a16148c2e30f11f
Julia Gelij <116667343+JuliaGelij@users.noreply.github.com>
2023-08-03 11:29:31 +0200

    3 task done

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,27 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// Użyj tablicy obiektów images w celu utworzenia elementów <img> umieszczonych w <li>.
+//Aby utworzyć znacznik użyj template strings i metody insertAdjacentHTML().
+
+// Wszystkie elementy galerii powinny być dodawane do DOM podczas jednej operacji.
+// Ulepsz galerię używając flexboxów lub gridów poprzez klasy CSS.
+
+const galleryList = images.map(
+  (image) =>
+    `<li class="gallery__list"> <img class="gallery__image" src=${image.url} alt=${image.alt}> </li>`
+);
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", galleryList);
